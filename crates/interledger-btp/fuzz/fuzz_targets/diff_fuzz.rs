47cc154f9c527c719ed4c7b5030b202176d8ca16
Joonas Koivunen <joonas.koivunen@gmail.com>
2021-03-13 18:52:16 +0200

    test(btp): diff fuzzing against bytes04 version
    Signed-off-by: Joonas Koivunen <joonas.koivunen@gmail.com>

---
+++ crates/interledger-btp/fuzz/fuzz_targets/diff_fuzz.rs
@@ -0,0 +1,68 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use std::fmt::{self, Write};
+
+use interledger_btp as curr;
+
+fuzz_target!(|data: &[u8]| {
+    if data.is_empty() {
+        // already fixed in HEAD
+        return;
+    }
+    let new = curr::fuzzing::verbose_roundtrip(data);
+    let old = prev::fuzzing::verbose_roundtrip(data);
+
+    match (new, old) {
+        (Ok((val, bytes)), Ok((old_val, old_bytes))) => {
+            assert_eq!(bytes, old_bytes);
+        }
+        (Err(e), Ok((old_val, _))) => {
+            panic!(
+                "rejected {:?} but old parsed {:?} with {}",
+                data, old_val, e
+            );
+        }
+        (Ok((val, _)), Err(e)) => {
+            panic!(
+                "accepted {:?} but old failed with {} for {:?}",
+                val, e, data
+            );
+        }
+        (Err(new), Err(old)) => {
+            check_display(
+                &new,
+                &old,
+                &[(
+                    "I/O Error: too short variable length octet string",
+                    "I/O Error: failed to fill whole buffer",
+                )],
+            );
+        }
+    }
+});
+
+#[allow(unused)]
+fn check_slices<A: AsRef<[u8]>, B: AsRef<[u8]>>(a: &A, b: &B) {
+    assert_eq!(a.as_ref(), b.as_ref());
+}
+
+#[allow(unused)]
+fn check_display<A: fmt::Display, B: fmt::Display>(a: &A, b: &B, allowed: &[(&str, &str)]) {
+    let mut s = String::new();
+
+    write!(s, "{}", a).expect("new impl failed to print itself");
+    let split_at = s.len();
+
+    write!(s, "{}", b).expect("old impl failed to print itself");
+
+    let left = &s[..split_at];
+    let right = &s[split_at..];
+
+    for &(l, r) in allowed {
+        if left == l && right == r {
+            return;
+        }
+    }
+
+    assert_eq!(left, right);
+}
